1a17885603493683acf7742892693a124dff4471
Martin <martinius96@gmail.com>
2019-01-26 12:03:02 +0100

    Update program.php

--- program.php
+++ program.php
@@ -49,10 +49,13 @@
 	 <hr><h2>Wiring</h2><hr>
 	 	 <img src="https://i.stack.imgur.com/e1ewN.png" style="display: block; max-width: 100%; height: auto;">
 	   <hr><h2>Projekt</h2><hr>
-	   <li><a href="https://ulozto.sk/!YUKP0EbHnv5U/rfid-zip">Knižnica pre RC522</a></li><br>
 	   <li><a href="https://github.com/esp8266/Arduino/">Návod na inštaláciu NodeMCU do ArduinoIDE</a></li><br>
-	  <hr><h2>Zdrojový kód</h2><hr>
-<pre style="background-color:powderblue;">
+	   <li><a href="http://www.handsontec.com/pdf_learn/esp8266-V10.pdf">NodeMCU v1.0 (v3, v2) datasheet</a></li><br>
+	   <li>Knižnica pre čítačku RC522 je obsiahnutá v repozitári</li><br>
+	   <li><a href="https://www.nxp.com/docs/en/data-sheet/MFRC522.pdf">RC522 datasheet</a></li><br>
+	   <li><a href="https://github.com/martinius96/RFID-otvaranie-dveri/">Repozitár free verzie projektu pod MIT licenciou</a></li>
+	   <hr><h2>Zdrojový kód - HTTPS</h2><hr>
+<pre style="background-color:#4cd137;">
 /*|----------------------------------------------------------|*/
 /*|SKETCH PRE RFID SYSTEM S WEB ADMINISTRACIOU               |*/
 /*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
@@ -67,70 +70,145 @@
 #include &lt;RFID.h&gt;
 const char * ssid = "MenoWifiSiete";
 const char * password = "HesloWifiSiete";
-const char * host = "example.com"; //bez https a www
+const char * host = "arduino.php5.sk"; //bez https a www
 const int httpsPort = 443; //https port
-const char * fingerprint = "‎a6 02 4d e1 32 b0 0b fe 56 85 0f 84 03 ec b2 18 23 09 f0 aa"; // odtlacok HTTPS
+const int rele = 16; //GPIO16 == D0
+const char * fingerprint = "‎a6 02 4d e1 32 b0 0b fe 56 85 0f 84 03 ec b2 18 23 09 f0 63"; // odtlacok HTTPS cert
 #define SS_PIN 4
 #define RST_PIN 5
-
 RFID rfid(SS_PIN, RST_PIN); 
 unsigned long kod;
- WiFiClientSecure client; //HTTPS client
-void setup()
-{ 
-  Serial.begin(9600);
-  SPI.begin(); 
-  rfid.init();
+WiFiClientSecure client; //HTTPS client
+void setup(){ 
+	Serial.begin(9600);
+  	SPI.begin(); 
+  	rfid.init();
+  	pinMode(rele, OUTPUT);
+	WiFi.begin(ssid, password);
+  	while (WiFi.status() != WL_CONNECTED) {
+    		delay(500);
+    		Serial.print(".");
+  	}
+	Serial.println("");
+  	Serial.println("WiFi uspesne pripojene");
+  	Serial.println("IP adresa: ");
+  	Serial.println(WiFi.localIP());
+  	Serial.println("Ready");
+}
+
+void loop(){
+  	if (WiFi.status() != WL_CONNECTED) {
+    		WiFi.begin(ssid, password);
+  	}
+  	while (WiFi.status() != WL_CONNECTED) {
+    		delay(500);
+    		Serial.print(".");
+  	}
+  	if (rfid.isCard()) {
+    		if (rfid.readCardSerial()) {
+      			Serial.println(" ");
+      			Serial.println("Card found");
+      			kod = 10000*rfid.serNum[4]+1000*rfid.serNum[3]+100*rfid.serNum[2]+10*rfid.serNum[1]+rfid.serNum[0];
+      			Serial.println(kod);
+      			String kodik = String(kod);
+      			client.stop();      
+      			if (client.connect(host, httpsPort)) {
+        			String url = "/rfid/karta.php?kod="+kodik;
+        			client.print(String("GET ") + url + " HTTP/1.0\r\n" + "Host: " + host + "\r\n" + "User-Agent: NodeMCU\r\n" + "Connection: close\r\n\r\n");
+      				while (client.connected()) {
+        				String line = client.readStringUntil('\n');
+        				if (line == "\r") {
+          					break;
+        				}	
+      				}
+  				String line = client.readStringUntil('\n');
+  				if (line == "OK"){
+	 				digitalWrite(rele, LOW); //invertovane spinane rele active LOW
+	 				delay(5500);              //cas otvorenia dveri
+  				}else if (line == "NO") {
+    					digitalWrite(rele,HIGH);
+				}
+  			}
+          }
+    	}
+	rfid.halt();
+}
+</pre>
+	  <hr><h2>Zdrojový kód - HTTP</h2><hr>
+<pre style="background-color:#e84118;">
+/*|----------------------------------------------------------|*/
+/*|SKETCH PRE RFID SYSTEM S WEB ADMINISTRACIOU               |*/
+/*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
+/*|EMAIL: martinius96@gmail.com                              |*/
+/*|Doska: NodeMCU v3 Lolin (v2 compatible)                   |*/
+/*|CORE: 2.3.0                                               |*/
+/*|WEB: https://arduino.php5.sk                              |*/
+/*|----------------------------------------------------------|*/
+#include &lt;ESP8266WiFi.h&gt;
+#include &lt;SPI.h&gt;
+#include &lt;RFID.h&gt;
+const char * ssid = "MenoWifiSiete";
+const char * password = "HesloWifiSiete";
+const char * host = "www.arduino.php5.sk"; 
+const int httpPort = 80; //http port
+const int rele = 16; //GPIO16 == D0
+#define SS_PIN 4
+#define RST_PIN 5
+RFID rfid(SS_PIN, RST_PIN); 
+unsigned long kod;
+WiFiClient client;
+void setup(){ 
+    Serial.begin(9600);
+    SPI.begin(); 
+    rfid.init();
+    pinMode(rele, OUTPUT);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi uspesne pripojene");
-  Serial.println("IP adresa: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Ready");
-
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi uspesne pripojene");
+    Serial.println("IP adresa: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Ready");
 }
 
-void loop()
-{
-if (WiFi.status() != WL_CONNECTED) {
-WiFi.begin(ssid, password);
-}
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+void loop(){
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
     if (rfid.isCard()) {
         if (rfid.readCardSerial()) {
-           
-                /* With a new cardnumber, show it. */
-                Serial.println(" ");
-                Serial.println("Card found");
-              kod = 10000*rfid.serNum[4]+1000*rfid.serNum[3]+100*rfid.serNum[2]+10*rfid.serNum[1]+rfid.serNum[0];
-          Serial.println(kod);
-          String kodik = String(kod);
-            if (client.connect(host, httpsPort)) {
-    String url = "/karta.php?kod="+kodik;
-    client.print(String("GET ") + url + " HTTP/1.0\r\n" + "Host: " + host + "\r\n" + "User-Agent: NodeMCU\r\n" + "Connection: close\r\n\r\n");
-
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        break;
-      }
-    }
-    String line = client.readStringUntil('\n');
-    if (line == "OK") {
-	//FUNKCNOST SA DOPLNI NESKOR
-    } else if (line == "NO") {
-    //FUNKCNOST SA DOPLNI NESKOR
-	}
-  }
+            Serial.println(" ");
+            Serial.println("Card found");
+            kod = 10000*rfid.serNum[4]+1000*rfid.serNum[3]+100*rfid.serNum[2]+10*rfid.serNum[1]+rfid.serNum[0];
+            Serial.println(kod);
+            String kodik = String(kod);
+            client.stop();
+            if (client.connect(host, httpPort)) {
+              String url = "/rfid/karta.php?kod="+kodik;
+              client.print(String("GET ") + url + " HTTP/1.0\r\n" + "Host: " + host + "\r\n" + "User-Agent: NodeMCU\r\n" + "Connection: close\r\n\r\n");
+              while (client.connected()) {
+                String line = client.readStringUntil('\n');
+                if (line == "\r") {
+                    break;
+                }
+              }
+          String line = client.readStringUntil('\n');
+          if (line == "OK"){
+          digitalWrite(rele, LOW); //invertovane spinane rele active LOW
+          delay(5500);              //cas otvorenia dveri
+            }else if (line == "NO") {
+                digitalWrite(rele,HIGH);
           }
-    }
+        }
+            }
+      }
 
     rfid.halt();
 }
