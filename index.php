96513965119ba26357ffc58fb83711554c8f0966
Martin <martinius96@gmail.com>
2019-04-01 17:55:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ include("connect.php");
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Martin Chlebovec">
+    <meta name="contact" content="martinius96@gmail.com">
 
     <title>Webaplikácia apartmánu</title>
 
