8765f2d2b9c04ea4d5e31fd02c21e7a3235b5ddf
Martin <martinius96@gmail.com>
2019-04-28 11:19:51 +0200

    Update grafy.php

--- grafy.php
+++ grafy.php
@@ -15,6 +15,14 @@ include("connect.php");
     <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <script type="text/javascript">
+    window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'db50efe9fff280a17db52b82be221240cbbd3dbe');
+</script>
 
   </head>
        <?php 
