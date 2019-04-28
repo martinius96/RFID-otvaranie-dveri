75e84b033aca3310c3d6d57feae66eaf83efaf92
Martin <martinius96@gmail.com>
2019-04-28 17:08:02 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,7 @@
 <?php 
 $con = mysqli_connect("localhost","root","pass","dbname");
-
-// Check connection
 if (mysqli_connect_errno($con))
-{
-    echo "Failed to connect to DataBase: " . mysqli_connect_error();
-}
+    {
+        echo "Problém s napojením na MySQL databázu: " . mysqli_connect_error();
+    }
 ?>
