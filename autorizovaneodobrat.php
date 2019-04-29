3cea23c36d5e969ba9683c21767e1aaf857e824a
Martin <martinius96@gmail.com>
2019-04-30 01:19:57 +0200

    Update autorizovaneodobrat.php

--- autorizovaneodobrat.php
+++ autorizovaneodobrat.php
@@ -2,18 +2,20 @@
 include("connect.php");
 ?>
 <table style="width: 100%;" border="1">
-									 <tr>
-									 <th style="width: 33.33%;">Autorizované od</th>
-									 <th style="width: 33.33%;">Číslo karty</th>
-									 <th style="width: 33.33%;">Akcia</th>
- </tr>
+	<tr>
+	<th style="width: 33.33%;">Autorizované od</th>
+	<th style="width: 33.33%;">Číslo karty</th>
+	<th style="width: 33.33%;">Akcia</th>
+</tr>
 <?php
  	$karty = mysqli_query($con,"SELECT * FROM autorizovane ORDER BY id DESC") or die(mysqli_error($con));
-		while($line = mysqli_fetch_assoc($karty)){
-			echo "<tr>";
-				$casik = date('d. M H:i:s',strtotime($line['time']));			
-       echo "<td><i>". $casik . "</i></td>";
-				  echo '<td><i>' . $line['cislo_karty'] .'</i></td>';
-				  echo "<td><a href='zmazat.php?".$line['id']."' class='btn btn-danger'>Zmazať</a></td>";
-			echo "</tr>";
-		}  ?> </tbody></table>
+	while($line = mysqli_fetch_assoc($karty)){
+		echo "<tr>";
+		$casik = date('d. M H:i:s',strtotime($line['time']));			
+       		echo "<td><i>". $casik . "</i></td>";
+		echo '<td><i>' . $line['cislo_karty'] .'</i></td>';
+		echo "<td><a href='zmazat.php?".$line['id']."' class='btn btn-danger'>Zmazať</a></td>";
+		echo "</tr>";
+	}  
+?> 
+</table>
