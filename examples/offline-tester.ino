a62a1134f708e7ff185d518c19394fec885dfd65
Martin <martinius96@gmail.com>
2019-05-09 10:30:33 +0200

    Create offline-tester.ino

---
+++ examples/offline-tester.ino
@@ -0,0 +1,33 @@
+/*|----------------------------------------------------------|*/
+/*|SKETCH PRE TEST RFID CITACKY RC522 S ESP8266              |*/
+/*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
+/*|EMAIL: martinius96@gmail.com                              |*/
+/*|Doska: NodeMCU v3 Lolin (v2 compatible)                   |*/
+/*|CORE: 2.3.0                                               |*/
+/*|WEB: https://arduino.php5.sk                              |*/
+/*|----------------------------------------------------------|*/
+#include <SPI.h>
+#include <RFID.h>
+#define SS_PIN 4
+#define RST_PIN 5
+RFID rfid(SS_PIN, RST_PIN); 
+unsigned long kod;
+
+void setup(){ 
+  Serial.begin(9600);
+  SPI.begin(); 
+  rfid.init();
+}
+
+void loop(){
+  if (rfid.isCard()) {
+    if (rfid.readCardSerial()) {
+      Serial.println(" ");
+      Serial.println("Kod karty ziskany: ");
+      kod = 10000*rfid.serNum[4]+1000*rfid.serNum[3]+100*rfid.serNum[2]+10*rfid.serNum[1]+rfid.serNum[0];
+      Serial.println(kod);
+      String kodik = String(kod);        
+    }
+  }
+  rfid.halt();
+}
