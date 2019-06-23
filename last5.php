d66ac77dd953638f0b01f463ae0b3127cadb4f97
Martin <martinius96@gmail.com>
2019-06-23 16:31:04 +0200

    format + html tbody del

--- last5.php
+++ last5.php
@@ -1,28 +1,29 @@
 <?php 
 include("connect.php");
 ?>
-    <table style="width: 100%;" border="1">
-									 <tr>
-									 <th style="width: 33.33%;">Čas</th>
-									 <th style="width: 33.33%;">Kľúčenka</th>
-									 <th style="width: 33.33%;">Výsledok</th>
-									 
-									 </tr>
+<table style="width: 100%;" border="1">
+	<tr>
+		<th style="width: 33.33%;">Čas</th>
+		<th style="width: 33.33%;">Kľúčenka</th>
+		<th style="width: 33.33%;">Výsledok</th>
+	</tr>
 <?php
- 	$karty = mysqli_query($con,"SELECT * FROM pokusy ORDER BY id DESC LIMIT 5") or die(mysqli_error($con));
-		while($line = mysqli_fetch_assoc($karty)){
-			echo "<tr>";
-				$casik = date('d. M H:i:s',strtotime($line['time']));	
-				if($line['vysledok']==1){
-				echo '<td bgcolor="#00FF00"><i>'. $casik . '</i></td>';	
-				echo '<td bgcolor="#00FF00"><i>' . $line['cislo_karty']. '</i></td>';
-				$vysledok = "Overený";
-				  echo '<td bgcolor="#00FF00"><i>' . $vysledok .'</i></td>';
-				}else{
-					echo '<td bgcolor="#FF0000"><i>'. $casik . '</i></td>';	
-				echo '<td bgcolor="#FF0000"><i>' . $line['cislo_karty']. '</i></td>';
-				$vysledok = "Neoverený";
-				  echo '<td bgcolor="#FF0000"><i>' . $vysledok .'</i></td>';
-				}		
-			echo "</tr>";
-		}  ?> </tbody></table>
+$karty = mysqli_query($con,"SELECT * FROM pokusy ORDER BY id DESC LIMIT 5") or die(mysqli_error($con));
+	while($line = mysqli_fetch_assoc($karty)){
+		echo "<tr>";
+		$casik = date('d. M H:i:s',strtotime($line['time']));	
+		if($line['vysledok']==1){
+			echo '<td bgcolor="#00FF00"><i>'. $casik . '</i></td>';	
+			echo '<td bgcolor="#00FF00"><i>' . $line['cislo_karty']. '</i></td>';
+			$vysledok = "Overený";
+			echo '<td bgcolor="#00FF00"><i>' . $vysledok .'</i></td>';
+		}else{
+			echo '<td bgcolor="#FF0000"><i>'. $casik . '</i></td>';	
+			echo '<td bgcolor="#FF0000"><i>' . $line['cislo_karty']. '</i></td>';
+			$vysledok = "Neoverený";
+			echo '<td bgcolor="#FF0000"><i>' . $vysledok .'</i></td>';
+		}		
+		echo "</tr>";
+	}  
+?> 
+</table>
