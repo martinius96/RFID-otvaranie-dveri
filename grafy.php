5c86d0493790c6e299d24656372f822de998e3ed
Martin <martinius96@gmail.com>
2019-06-25 01:35:29 +0200

    Add files via upload

--- grafy.php
+++ grafy.php
@@ -93,6 +93,7 @@ $jsonTable = json_encode($table);
        
         </div>
       </div>
+      <center><footer style="background: #D35400;"><font color="white">Vytvoril a držitelom MIT licencie je: </font><a href="https://www.facebook.com/martin.s.chlebovec"><font color="white">Martin Chlebovec</font></a></footer></center>
     </div>
 
     <!-- Bootstrap core JavaScript -->
