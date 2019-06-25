74d5650cd2f35a32c71d34ae125ef125c3ee3525
Martin Chlebovec <martin.chlebovec.2@student.tuke.sk>
2019-06-25 15:11:52 +0200

    odstranenie projektu pred porusovatelmi licencie

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$con = mysqli_connect("localhost","pouzivatel","heslo","nazov_databazy");
-if (mysqli_connect_errno($con))
-    {
-        echo "Problém s napojením na MySQL databázu: " . mysqli_connect_error();
-    }
-?>
--- zmazat.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include("connect.php");
-$zmaz = mysqli_query($con,"DELETE FROM `autorizovane` WHERE `id`='".$_SERVER['QUERY_STRING']."'") or die(mysqli_error($con));
-header("Location: odobrat.php");
-?>
