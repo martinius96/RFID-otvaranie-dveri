562900dfc37b3292d5114f152280c01aa3ec9ff4
Martin <martinius96@gmail.com>
2019-06-28 12:11:23 +0200

    Update tester.ino

--- src/rfid/examples/tester/tester.ino
+++ src/rfid/examples/tester/tester.ino
@@ -2,9 +2,10 @@
 /*|SKETCH PRE TEST RFID CITACKY RC522 S ESP8266              |*/
 /*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
 /*|EMAIL: martinius96@gmail.com                              |*/
-/*|Doska: NodeMCU v3 Lolin (v2 compatible)                   |*/
+/*|DOSKA: NodeMCU v3 Lolin (v2 compatible)                   |*/
 /*|CORE: 2.3.0                                               |*/
 /*|WEB: https://arduino.php5.sk                              |*/
+/*|LICENCIA: MIT                                             |*/
 /*|----------------------------------------------------------|*/
 #include <SPI.h>
 #include <RFID.h>
