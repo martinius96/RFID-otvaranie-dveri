45b10f34f7e49ba08664724423f568c5cd6273b7
Martin <martinius96@gmail.com>
2019-07-13 22:25:00 +0200

    updated

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-$con = mysqli_connect("localhost","pouzivatel_db_root","heslo_do_databazy","pouzivana_databaza");
+$con = mysqli_connect("localhost/EXTERNAL_DB_HOST","pouzivatel_db_root","heslo_do_databazy","pouzivana_databaza");
 if (mysqli_connect_errno($con))
     {
         echo "Problém s napojením na MySQL databázu: " . mysqli_connect_error();
