6213bea5ac7a124bfb403df096b44b060753ba0b
Martin <martinius96@gmail.com>
2019-07-13 22:18:46 +0200

    updated

--- pridaj.php
+++ pridaj.php
@@ -2,5 +2,6 @@
   include("connect.php");
   $ins7 = mysqli_query($con,"INSERT INTO `autorizovane` (`cislo_karty`) VALUES ('".$_SERVER['QUERY_STRING']."')") or die (mysqli_error($con));
   $zmaz = mysqli_query($con,"DELETE FROM `neautorizovane` WHERE `cislo_karty`='".$_SERVER['QUERY_STRING']."'") or die(mysqli_error($con));
+  echo "OK";  
   header("Location: pridat.php");
 ?>
