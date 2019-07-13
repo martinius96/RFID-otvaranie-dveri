8b828d69d4df136dcbd0b2c89f3d77d799726a6c
Martin <martinius96@gmail.com>
2019-07-13 21:54:22 +0200

    format

--- zapojenie.php
+++ zapojenie.php
@@ -44,12 +44,15 @@ $stranka = "Zapojenie";
 
     <!-- Page Content -->
     <div class="container">
-	 <hr><h2>Zapojenie</h2><hr>
-   <b>Stiahnuť: </b>
-   <li>.sch - <a href="eagle/rfid_otvaranie_dveri.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle" alt="Autodesk Eagle"></a></li>
-   <li>.dxf - <a href="eagle/rfid_otvaranie_dveri.dxf"><img src="https://www.shareicon.net/data/256x256/2017/06/21/887531_file_512x512.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD" alt="CAD - AutoCAD, SoliCAD, ProgeCAD"></a></li>
-	 	 <img src="https://i.imgur.com/j9wciSz.png" style="display: block; max-width: 100%; height: auto;">
-	   </div>
+	<b>Dostupné na stiahnutie: </b>
+   	<li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle" alt="Autodesk Eagle"></a></li>
+   	<hr>
+   	<li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD" alt="CAD - AutoCAD, SoliCAD, ProgeCAD"></a></li>
+   	<hr>
+   	<li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript" alt="NetScript"></a></li>
+	<hr><h2>Zapojenie:</h2>
+	<img src="https://i.imgur.com/j9wciSz.png" style="display: block; max-width: 100%; height: auto;">
+    </div>
      <?php 
       include("footer.php");
       ?>
