82362e8c9bfd2940313926e830ebb10a0d2a38d9
Martin <martinius96@gmail.com>
2019-07-13 21:26:25 +0200

    calc

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<tr>
 		<th style="width: 33.33%;"><center><font color="#566573">Programovo vyhotovil: <a href="mailto:martinius96@gmail.com">Martin Chlebovec</font></a></center></th>
 		<th style="width: 33.33%;"><center><font color="#566573">Licencia použitia projektu: <a href="https://github.com/martinius96/RFID-otvaranie-dveri/blob/master/LICENSE">MIT</a></font></center></th>
-		<th style="width: 33.33%;"><center><font color="#566573">Frontend + Grid systém: Bootstrap</font></center></th>									 
+		<th style="width: 33.34%;"><center><font color="#566573">Frontend + Grid systém: Bootstrap</font></center></th>									 
   </tr>
 </table>
 </footer>
