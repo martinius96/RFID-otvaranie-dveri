77c5d30771057872c992c1dc425871838f47d00b
Martin <martinius96@gmail.com>
2019-07-17 13:03:27 +0200

    esp32 sketch

---
+++ examples/http-rfid-esp32-sketch.ino
@@ -0,0 +1,80 @@
+/*|----------------------------------------------------------|*/
+/*|SKETCH PRE RFID SYSTEM S WEB ADMINISTRACIOU               |*/
+/*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
+/*|FB: https://www.facebook.com/martin.s.chlebovec           |*/
+/*|EMAIL: martinius96@gmail.com                              |*/
+/*|Doska: ESP32 DevKit V1 - V4                               |*/
+/*|CORE: 1.0.0+                                              |*/
+/*|WEB: https://arduino.php5.sk                              |*/
+/*|----------------------------------------------------------|*/
+#include <WiFi.h>
+#include <SPI.h>
+#include <RFID.h>
+const char * ssid = "wifi_meno";
+const char * password = "wifi_heslo";
+const char * host = "www.arduino.php5.sk";
+const int httpPort = 80; //http port
+const int rele = 17;
+#define SS_PIN 21
+#define RST_PIN 22
+RFID rfid(SS_PIN, RST_PIN);
+unsigned long kod;
+WiFiClient client;
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  rfid.init();
+  pinMode(rele, OUTPUT);
+  digitalWrite(rele, HIGH); //hotfix
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi uspesne pripojene");
+  Serial.println("IP adresa: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Ready");
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  if (rfid.isCard()) {
+    if (rfid.readCardSerial()) {
+      Serial.println(" ");
+      Serial.println("Card found");
+      kod = 10000 * rfid.serNum[4] + 1000 * rfid.serNum[3] + 100 * rfid.serNum[2] + 10 * rfid.serNum[1] + rfid.serNum[0];
+      Serial.println(kod);
+      String kodik = String(kod);
+      client.stop();
+      if (client.connect(host, httpPort)) {
+        String url = "/rfid/karta.php?kod=" + kodik;
+        //String url = "/rfid/karta.php?kod="+kodik+"&origin=ESP32";
+        //String url = "/rfid/karta.php?kod="+kodik+"&origin=ESP32&topsecret=topsecret";
+        client.print(String("GET ") + url + " HTTP/1.0\r\n" + "Host: " + host + "\r\n" + "User-Agent: ESP32\r\n" + "Connection: close\r\n\r\n");
+        while (client.connected()) {
+          String line = client.readStringUntil('\n');
+          if (line == "\r") {
+            break;
+          }
+        }
+        String line = client.readStringUntil('\n');
+        if (line == "OK") {
+          digitalWrite(rele, LOW); //invertovane spinane rele active LOW
+          delay(5500);              //cas otvorenia dveri
+        } else if (line == "NO") {
+          digitalWrite(rele, HIGH);
+        }
+      }
+    }
+  }
+
+  rfid.halt();
+}
