ccd9b464fbc0b2d63f240e66382845c96bccb274
Martin <martinius96@gmail.com>
2019-07-17 12:04:28 +0200

    Update zapojenie.php

--- zapojenie.php
+++ zapojenie.php
@@ -10,7 +10,7 @@ $stranka = "Zapojenie";
     <meta name="keywords" content="zapojenie, arduino, arduinoide, nodemcu, esp8266, čip, iot, rfid, vrátnik, rc522, relé, solenoid, dvere, jazýček, ovládanie, internet, ethernet">
     <meta name="author" content="Martin Chlebovec">
     <meta name="robots" content="index, follow">
-    <title>RFID vrátnik - ESP8266 - Zapojenie</title>
+    <title>RFID vrátnik - Zapojenie komponentov</title>
      <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -40,26 +40,33 @@ $stranka = "Zapojenie";
         </div>
       </div>
     </nav>
-
     <!-- Page Content -->
     <div class="container">
   <hr><h2>Zapojenie - ESP8266</h2>
    <img src="https://i.imgur.com/j9wciSz.png" style="display: block; max-width: 100%; height: auto;" title="RFID vrátnik - schéma ESP8266" alt="RFID vrátnik - schéma ESP8266">
    <hr><h2>Zapojenie - Arduino + Ethernet W5100</h2>
    <img src="https://i.imgur.com/R4d2FvF.png" style="display: block; max-width: 100%; height: auto;" title="RFID vrátnik - schéma Arduino + W5100 Ethernet" alt="RFID vrátnik - schéma Arduino + W5100 Ethernet">
-	 <b>Dostupné na stiahnutie (ESP8266): </b>
-   <li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle" alt="Autodesk Eagle"></a></li>
+  <hr><h2>Zapojenie - ESP32</h2>
+   <img src="https://i.imgur.com/HKHrsEO.png" style="display: block; max-width: 100%; height: auto;" title="RFID vrátnik - schéma ESP32" alt="RFID vrátnik - schéma ESP32">
+   <b>Dostupné na stiahnutie (ESP8266): </b>
+   <li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle - ESP8266 (NodeMCU)" alt="Autodesk Eagle - ESP8266 (NodeMCU)"></a></li>
    <hr>
-   <li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD" alt="CAD - AutoCAD, SoliCAD, ProgeCAD"></a></li>
+   <li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD - ESP8266 (NodeMCU)" alt="CAD - AutoCAD, SoliCAD, ProgeCAD - ESP8266 (NodeMCU)"></a></li>
    <hr>
-   <li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript" alt="NetScript"></a></li>
-	 <b>Dostupné na stiahnutie (Arduino + Ethernet W5100): </b>
-   <li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri_uno.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle" alt="Autodesk Eagle"></a></li>
+   <li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript - ESP8266 (NodeMCU)" alt="NetScript - ESP8266 (NodeMCU)"></a></li>
+   <b>Dostupné na stiahnutie (Arduino + Ethernet W5100): </b>
+   <li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri_uno.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle - Arduino + Ethernet" alt="Autodesk Eagle - Arduino + Ethernet"></a></li>
    <hr>
-   <li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri_uno.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD" alt="CAD - AutoCAD, SoliCAD, ProgeCAD"></a></li>
+   <li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri_uno.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD - Arduino + Ethernet" alt="CAD - AutoCAD, SoliCAD, ProgeCAD - Arduino + Ethernet"></a></li>
    <hr>
-   <li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri_uno.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript" alt="NetScript"></a></li>  
-    <hr></div>
+   <li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri_uno.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript - Arduino + Ethernet" alt="NetScript - Arduino + Ethernet"></a></li>  
+   <b>Dostupné na stiahnutie (ESP32): </b>
+   <li>Formát .sch - <a href="eagle/rfid_otvaranie_dveri_esp32.sch"><img src="https://d2.alternativeto.net/dist/icons/eaglecad_116263.png?width=128&height=128&mode=crop&upscale=false" width="32px" height="32px" title="Autodesk Eagle - ESP32" alt="Autodesk Eagle - ESP32"></a></li>
+   <hr>
+   <li>Formát .dxf - <a href="eagle/rfid_otvaranie_dveri_esp32.dxf"><img src="http://www.iconninja.com/files/424/698/454/file-document-extension-cad-format-page-icon.png" width="32px" height="32px" title="CAD - AutoCAD, SoliCAD, ProgeCAD - ESP32" alt="CAD - AutoCAD, SoliCAD, ProgeCAD - ESP32"></a></li>
+   <hr>
+   <li>Formát .scr - <a href="eagle/rfid_otvaranie_dveri_esp32.scr"><img src="https://cdn0.iconfinder.com/data/icons/flat-design-database-set-3/24/script-128.png" width="32px" height="32px" title="NetScript - ESP32" alt="NetScript - ESP32"></a></li>
+   <hr></div>
      <?php 
       include("footer.php");
       ?>
