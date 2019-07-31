fb58bbf490e1f01a2f2d3c16595cf9b5381ec817
Martin <martinius96@gmail.com>
2019-07-31 16:15:30 +0200

    Update program.php

--- program.php
+++ program.php
@@ -29,7 +29,7 @@ $stranka = "Program";
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
       <div class="container">
-        <a class="navbar-brand" href="index.php">Webaplikácia - RFID vrátnik cez ESP8266</a>
+    <a class="navbar-brand" href="index.php">RFID vrátnik</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
