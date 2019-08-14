c49d26767a8136adae0100a7ee800b2f427a1c77
Martin <martinius96@gmail.com>
2019-08-14 20:01:19 +0200

    Update neautorizovane.php

--- neautorizovane.php
+++ neautorizovane.php
@@ -2,23 +2,18 @@
 include("connect.php");
 ?>
 <table style="width: 100%;" border="1">
-	<thead>
-		<tr>
-			<th style="width: 33.33%;">Od</th>
-			<th style="width: 33.33%;">Číslo karty</th>
-			<th style="width: 33.34%;">Akcia</th>
- 		</tr>
-	</thead>
-  	<tbody>
+									 <tr>
+									 <th style="width: 33.33%;">Od</th>
+									 <th style="width: 33.33%;">Číslo karty</th>
+									 <th style="width: 33.33%;">Akcia</th>
+ </tr>
 <?php
  	$karty = mysqli_query($con,"SELECT * FROM neautorizovane GROUP BY cislo_karty") or die(mysqli_error($con));
 		while($line = mysqli_fetch_assoc($karty)){
 			echo "<tr>";
 			$casik = date('d. M H:i:s',strtotime($line['time']));			
-      			echo "<td><i>". $casik . "</i></td>";
-			echo '<td><i>' . $line['cislo_karty'] .'</i></td>';
-			echo "<td><a href='pridaj.php?".$line['cislo_karty']."' class='btn btn-success'>Pridať</a></td>";
+       echo "<td><i>". htmlspecialchars($casik) . "</i></td>";
+				  echo '<td><i>' . htmlspecialchars($line['cislo_karty']) .'</i></td>';
+				 echo "<td><a href='pridaj.php?".htmlspecialchars($line['cislo_karty'])."' class='btn btn-success'>Pridať</a></td>";
 			echo "</tr>";
-		}  ?> 
-	</tbody>
-</table>
+		}  ?> </tbody></table>
