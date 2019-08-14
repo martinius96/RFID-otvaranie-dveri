71bfd8e2b676010c9ffddc4bc35363aceade2ad4
Martin <martinius96@gmail.com>
2019-08-14 15:03:12 +0200

    hotfix

--- pridaj.php
+++ pridaj.php
@@ -1,7 +1,10 @@
 <?php 
   include("connect.php");
-  $ins7 = mysqli_query($con,"INSERT INTO `autorizovane` (`cislo_karty`) VALUES ('".$_SERVER['QUERY_STRING']."')") or die (mysqli_error($con));
-  $zmaz = mysqli_query($con,"DELETE FROM `neautorizovane` WHERE `cislo_karty`='".$_SERVER['QUERY_STRING']."'") or die(mysqli_error($con));
-  echo "OK";  
+  $cislo_karty = mysqli_real_escape_string($con, $_SERVER['QUERY_STRING']);
+  $cislo_karty = htmlspecialchars($cislo_karty);
+  if (is_numeric($cislo_karty)){
+    $ins7 = mysqli_query($con,"INSERT INTO `autorizovane` (`cislo_karty`) VALUES ('".$cislo_karty."')") or die (mysqli_error($con));
+    $zmaz = mysqli_query($con,"DELETE FROM `neautorizovane` WHERE `cislo_karty`='".$cislo_karty."'") or die(mysqli_error($con));
+  }
   header("Location: pridat.php");
 ?>
