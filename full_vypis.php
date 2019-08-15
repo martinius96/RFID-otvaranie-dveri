7ec3b29f0802c344b944bf668498155fc050d0fb
Martin <martinius96@gmail.com>
2019-08-16 01:04:15 +0200

    Update full_vypis.php

--- full_vypis.php
+++ full_vypis.php
@@ -3,30 +3,28 @@ include("connect.php");
 ?>
 <table style="width: 100%;" border="1">
 	<thead>
-		<tr>
-			<th style="width: 33.33%;">Čas</th>
-			<th style="width: 33.33%;">Kľúčenka</th>
-			<th style="width: 33.34%;">Výsledok</th>									 
-		</tr>
-	</thead>
-  	<tbody>
+  <tr>
+		<th style="width: 33.33%;">Čas</th>
+		<th style="width: 33.33%;">Kľúčenka</th>
+		<th style="width: 33.33%;">Výsledok</th>
+									 
+	</tr></thead><tbody>
 <?php
  	$karty = mysqli_query($con,"SELECT * FROM pokusy ORDER BY id DESC") or die(mysqli_error($con));
 		while($line = mysqli_fetch_assoc($karty)){
 			echo "<tr>";
 			$casik = date('d. M H:i:s',strtotime($line['time']));	
 			if($line['vysledok']==1){
-				echo '<td bgcolor="#2ECC71"><i>'. htmlspecialchars($casik). '</i></td>';	
-				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($line['cislo_karty']). '</i></td>';
+				echo '<td bgcolor="#2ECC71"><i>'. htmlspecialchars($casik) . '</i></td>';	
+				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($line['cislo_karty']) . '</i></td>';
 				$vysledok = "Úspešné overenie";
 				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($vysledok) .'</i></td>';
 			}else{
 				echo '<td bgcolor="#E74C3C"><i>'. htmlspecialchars($casik) . '</i></td>';	
-				echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($line['cislo_karty']). '</i></td>';
+				echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($line['cislo_karty']) . '</i></td>';
 				$vysledok = "Neúspešné overenie";
 				echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($vysledok) .'</i></td>';
 			}		
 	echo "</tr>";
 		}  ?> 
-	</tbody>
-</table>
+</tbody></table>
