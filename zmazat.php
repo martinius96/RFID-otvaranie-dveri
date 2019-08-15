b765355907ed8697c9be5ab349575ce1c2aaae25
Martin <martinius96@gmail.com>
2019-08-16 00:56:25 +0200

    Update zmazat.php

--- zmazat.php
+++ zmazat.php
@@ -2,8 +2,12 @@
   include("connect.php");
   $cislo_karty = mysqli_real_escape_string($con, $_SERVER['QUERY_STRING']);
   $cislo_karty = htmlspecialchars($cislo_karty);
+  $cislo_karty = trim($cislo_karty);
   if (is_numeric($cislo_karty)){
-    $zmaz = mysqli_query($con,"DELETE FROM `autorizovane` WHERE `cislo_karty`='".$cislo_karty."'") or die(mysqli_error($con));
+      $over = mysqli_query($con,"SELECT * FROM `autorizovane` WHERE `cislo_karty`='$cislo_karty'") or die(mysqli_error($con));
+ 	    if(mysqli_num_rows($over) > 0){
+          $zmaz = mysqli_query($con,"DELETE FROM `autorizovane` WHERE `cislo_karty`='".$cislo_karty."'") or die(mysqli_error($con));
+      } 
   }
   header("Location: odobrat.php");
 ?>
