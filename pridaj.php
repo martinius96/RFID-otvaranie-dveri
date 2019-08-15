eb16c0999863174410c2511dec9e94d7af23a871
Martin <martinius96@gmail.com>
2019-08-16 00:55:37 +0200

    Update pridaj.php

--- pridaj.php
+++ pridaj.php
@@ -2,9 +2,13 @@
   include("connect.php");
   $cislo_karty = mysqli_real_escape_string($con, $_SERVER['QUERY_STRING']);
   $cislo_karty = htmlspecialchars($cislo_karty);
+  $cislo_karty = trim($cislo_karty);
   if (is_numeric($cislo_karty)){
-    $ins7 = mysqli_query($con,"INSERT INTO `autorizovane` (`cislo_karty`) VALUES ('".$cislo_karty."')") or die (mysqli_error($con));
-    $zmaz = mysqli_query($con,"DELETE FROM `neautorizovane` WHERE `cislo_karty`='".$cislo_karty."'") or die(mysqli_error($con));
+      $over = mysqli_query($con,"SELECT * FROM `neautorizovane` WHERE `cislo_karty`='$cislo_karty'") or die(mysqli_error($con));
+ 	    if(mysqli_num_rows($over) > 0){
+          $ins7 = mysqli_query($con,"INSERT INTO `autorizovane` (`cislo_karty`) VALUES ('".$cislo_karty."')") or die (mysqli_error($con));
+          $zmaz = mysqli_query($con,"DELETE FROM `neautorizovane` WHERE `cislo_karty`='".$cislo_karty."'") or die(mysqli_error($con));
+      }
   }
   header("Location: pridat.php");
 ?>
