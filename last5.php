322dc5e869441d83d14ab4f261005c5e24652077
Martin <martinius96@gmail.com>
2019-08-24 01:58:48 +0200

    Mena zamestnancov

--- last5.php
+++ last5.php
@@ -1,28 +1,36 @@
 <?php 
 include("connect.php");
 ?>
-    <table style="width: 100%;" border="1">
-									 <thead><tr>
-									 <th style="width: 33.33%;">Čas</th>
-									 <th style="width: 33.33%;">Kľúčenka</th>
-									 <th style="width: 33.33%;">Výsledok</th>
-									 
-									 </tr></thead><tbody>
+<table style="width: 100%;" border="1">
+	<thead>
+  <tr>
+		<th style="width: 25%;">Čas</th>
+		<th style="width: 25%;">Kľúčenka</th>
+		<th style="width: 25%;">Výsledok</th>
+		<th style="width: 25%;">Meno</th>							 
+	</tr></thead><tbody>           
 <?php
  	$karty = mysqli_query($con,"SELECT * FROM pokusy ORDER BY id DESC LIMIT 5") or die(mysqli_error($con));
 		while($line = mysqli_fetch_assoc($karty)){
 			echo "<tr>";
-				$casik = date('d. M H:i:s',strtotime($line['time']));	
-				if($line['vysledok']==1){
+			$casik = date('d. M H:i:s',strtotime($line['time']));	
+			if($line['vysledok']==1){
 				echo '<td bgcolor="#2ECC71"><i>'. htmlspecialchars($casik) . '</i></td>';	
-				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($line['cislo_karty']) . '</i></td>';
+				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($line['cislo_karty']) . '</i></td>';    
 				$vysledok = "Úspešné overenie";
-				  echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($vysledok) .'</i></td>';
-				}else{
-					echo '<td bgcolor="#E74C3C"><i>'. htmlspecialchars($casik) . '</i></td>';	
+				echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($vysledok) .'</i></td>';
+        $zamestnanec = mysqli_query($con,"SELECT meno FROM `zamestnanci` WHERE `cislo_karty`='".$line['cislo_karty']."'") or die(mysqli_error($con));
+        $line2 = mysqli_fetch_assoc($zamestnanec);
+        echo '<td bgcolor="#2ECC71"><i>' . htmlspecialchars($line2['meno']) . '</i></td>';
+			}else{
+				echo '<td bgcolor="#E74C3C"><i>'. htmlspecialchars($casik) . '</i></td>';	
 				echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($line['cislo_karty']) . '</i></td>';
 				$vysledok = "Neúspešné overenie";
-				  echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($vysledok) .'</i></td>';
-				}		
-			echo "</tr>";
-		}  ?> </tbody></table>
+				echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($vysledok) .'</i></td>';
+        $zamestnanec = mysqli_query($con,"SELECT meno FROM `zamestnanci` WHERE `cislo_karty`='".$line['cislo_karty']."'") or die(mysqli_error($con));
+        $line2 = mysqli_fetch_assoc($zamestnanec);
+        echo '<td bgcolor="#E74C3C"><i>' . htmlspecialchars($line2['meno']) . '</i></td>';
+			}		
+	echo "</tr>";
+		}  ?> 
+</tbody></table>
