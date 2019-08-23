43febaefab4a8b781f218decc91e8fda03366f6e
Martin <martinius96@gmail.com>
2019-08-24 01:59:55 +0200

    Update autorizovane.php

--- autorizovane.php
+++ autorizovane.php
@@ -3,17 +3,21 @@ include("connect.php");
 ?>
 <table style="width: 100%;" border="1">
 	<tr>
-		<th style="width: 50%;">Od</th>
-		<th style="width: 50%;">Číslo karty</th>
+		<th style="width: 33.33%;">Od</th>
+		<th style="width: 33.33%;">Číslo karty</th>
+    <th style="width: 33.33%;">Meno</th>
 	</tr>
 <?php
 	$karty = mysqli_query($con,"SELECT * FROM autorizovane ORDER BY id DESC") or die(mysqli_error($con));
 		while($line = mysqli_fetch_assoc($karty)){
 			echo "<tr>";
 			$casik = date('d. M H:i:s',strtotime($line['time']));			
-       			echo "<td><i>". htmlspecialchars($casik) . "</i></td>";
-			echo '<td><i>' . htmlspecialchars($line['cislo_karty']) .'</i></td>';
+       			echo "<td><i>". $casik . "</i></td>";
+			echo '<td><i>' . $line['cislo_karty'] .'</i></td>';
+      $zamestnanec = mysqli_query($con,"SELECT meno FROM `zamestnanci` WHERE `cislo_karty`='".$line['cislo_karty']."'") or die(mysqli_error($con));
+        $line2 = mysqli_fetch_assoc($zamestnanec);
+        echo '<td><i>' . htmlspecialchars($line2['meno']) . '</i></td>';
 			echo "</tr>";
-		}  
+}  
 ?> 
 </table>
