e21ec3dce37c98aeb81c55711b352007072a6012
Martin <martinius96@gmail.com>
2019-08-24 02:01:20 +0200

    fix pre diakritiku

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php 
 $con = mysqli_connect("localhost/EXTERNAL_DB_HOST","pouzivatel_db_root","heslo_do_databazy","pouzivana_databaza");
+mysqli_set_charset($con, "utf8");
 if (mysqli_connect_errno($con))
     {
         echo "Problém s napojením na MySQL databázu: " . mysqli_connect_error();
