4930a06171bc4bb93843ef5501b996e48a0ff076
Martin <martinius96@gmail.com>
2019-08-26 02:01:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,9 @@ include("connect.php");
     <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
      <script type="text/javascript">
     window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
@@ -24,10 +27,10 @@ include("connect.php");
     c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
     smartlook('init', 'db50efe9fff280a17db52b82be221240cbbd3dbe');
-</script>
+</script>    
   </head>
 
-  <body>
+  <body onload="myFunction()">
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
@@ -51,17 +54,13 @@ include("connect.php");
       </div>
       <div class="col-lg-12 text-center">
        <div id="full"></div>
+       <h1 id="demo"></h1>
         </div>
       </div>
       <?php 
       include("footer.php");
       ?>
-    </div>
-    
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+    </div>    
   </body>
     <script>
   $(function() {
