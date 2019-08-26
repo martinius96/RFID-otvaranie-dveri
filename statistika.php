5c3cc8196e7fb6aa1ebb702160e6cfd8704b16ac
Martin <martinius96@gmail.com>
2019-08-26 02:07:35 +0200

    Update statistika.php

--- statistika.php
+++ statistika.php
@@ -3,26 +3,31 @@ $stranka = "Stats";
 ?>
 <!DOCTYPE html>
 <?php include("connect.php"); ?>
-<html lang="sk">
+<html lang="en">
   <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Štatistika pre RFID vrátnika. Počet autorizovaných a neautorizovaných kariet">
-    <meta name="keywords" content="internet, esp32, esp, espressif, rfid, rc522, vrátnik, brána, vstup, kontrola, štatistika">
+  <meta name="description" content="Štatistika pre RFID vrátnika. Počet autorizovaných a neautorizovaných kariet">
+    <meta name="keywords" content="internet, esp32, esp, espressif, rfid, rc522, vrátnik, brána, vstup, kontrola, štatistika, karta, mifare">
     <meta name="author" content="Martin Chlebovec">
     <meta name="robots" content="index, follow">
     <title>RFID vrátnik - štatistika vstupov</title>
-    <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
+     <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script type="text/javascript">
-      window.smartlook||(function(d) {
-      var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-      var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-      c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
-      })(document);
-      smartlook('init', 'db50efe9fff280a17db52b82be221240cbbd3dbe');
-    </script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+     <script type="text/javascript">
+     <script type="text/javascript">
+    window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'db50efe9fff280a17db52b82be221240cbbd3dbe');
+</script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   </head>
   <?php 
@@ -84,12 +89,12 @@ $stranka = "Stats";
         chart.draw(data, options);
       }
     </script>
-  <body>
+  <body onload="myFunction()">
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
       <div class="container">
-        <a class="navbar-brand" href="index.php">RFID vrátnik</a>
+       <a class="navbar-brand" href="index.php">RFID vrátnik</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -103,6 +108,7 @@ $stranka = "Stats";
 
     <!-- Page Content -->
     <div class="container">
+      <h1 id="demo"></h1>
   <center><h2>Štatistika RFID vrátnika</h2></center>
     <li><b>Počet záznamov: </b><?php echo $cislom_vsetky; ?></li>
     <li><b>Počet vstupov: </b><?php echo $cislom_vstup; ?></li>
@@ -122,10 +128,6 @@ $stranka = "Stats";
       include("footer.php");
       ?>
       </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </body>
 
