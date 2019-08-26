ee1a98fd369a52ea31b10759a3cc3938214d1665
Martin <martinius96@gmail.com>
2019-08-26 02:03:37 +0200

    Update zapojenie.php

--- zapojenie.php
+++ zapojenie.php
@@ -14,17 +14,20 @@ $stranka = "Zapojenie";
      <link rel="icon" type="image/png" href="https://i.nahraj.to/f/2g8C.png" />
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script type="text/javascript">
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+     <script type="text/javascript">
     window.smartlook||(function(d) {
     var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
     var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
     c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
     })(document);
     smartlook('init', 'db50efe9fff280a17db52b82be221240cbbd3dbe');
-</script>
+</script>    
   </head>
 
-  <body>
+  <body onload="myFunction()">
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
@@ -42,6 +45,7 @@ $stranka = "Zapojenie";
     </nav>
     <!-- Page Content -->
     <div class="container">
+  <h1 id="demo"></h1>
   <hr><h2>Zapojenie - ESP8266</h2>
    <img src="https://i.imgur.com/j9wciSz.png" style="display: block; max-width: 100%; height: auto;" title="RFID vrátnik - schéma ESP8266" alt="RFID vrátnik - schéma ESP8266">
    <hr><h2>Zapojenie - Arduino + Ethernet W5100</h2>
@@ -54,10 +58,6 @@ $stranka = "Zapojenie";
       include("footer.php");
       ?>
      </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </body>
 
