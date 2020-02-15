a7b0679701551e70aa5811025dc8a5a7be142c4f
Martin <martinius96@gmail.com>
2020-02-16 00:35:45 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <footer>
-	<table style="width: 100%;" border="0">
-		<tr>
-			<th style="width: 25%;"><center><font color="#566573">Programovo vyhotovil: <a href="mailto:martinius96@gmail.com" id="a">Martin Chlebovec</font></a></center></th>
-			<th style="width: 25%;"><center><font color="#566573">Licencia použitia projektu: <a href="https://github.com/martinius96/RFID-otvaranie-dveri/blob/master/LICENSE" id="b">MIT</a></font></center></th>
-			<th style="width: 25%;"><center><font color="#566573">Frontend + Grid systém: Bootstrap</font></center></th>					
-			<th style="width: 25%;"><center><a href="https://www.facebook.com/martin.s.chlebovec"><img src="https://www.flaticon.com/premium-icon/icons/svg/2392/2392485.svg" style="height: 24px; width: 24px;"></a>
-			<a href="https://github.com/martinius96/RFID-otvaranie-dveri/blob/master/LICENSE"><img src="https://image.flaticon.com/icons/svg/2111/2111432.svg" style="height: 24px; width: 24px;"></a>
-			<a href="https://paypal.me/chlebovec/1"><img src="https://image.flaticon.com/icons/svg/217/217422.svg" style="height: 24px; width: 24px;"></a></center></th>							 
-		</tr>
-	</table>
+<table style="width: 100%;" border="0">
+<tr>
+<th style="width: 25%;"><center><font color="#566573">Programovo vyhotovil: <a href="mailto:martinius96@gmail.com" id="a">Martin Chlebovec</font></a></center></th>
+<th style="width: 25%;"><center><font color="#566573">Licencia použitia projektu: <a href="https://github.com/martinius96/RFID-otvaranie-dveri/blob/master/LICENSE" id="b">MIT</a></font></center></th>
+<th style="width: 25%;"><center><font color="#566573">Frontend + Grid systém: Bootstrap</font></center></th>					
+<th style="width: 25%;"><center><a href="https://www.facebook.com/martin.s.chlebovec"><img src="https://www.flaticon.com/premium-icon/icons/svg/2392/2392485.svg" style="height: 24px; width: 24px;"></a>
+<a href="https://github.com/martinius96/RFID-otvaranie-dveri/blob/master/LICENSE"><img src="https://image.flaticon.com/icons/svg/2111/2111432.svg" style="height: 24px; width: 24px;"></a>
+<a href="https://paypal.me/chlebovec/1"><img src="https://image.flaticon.com/icons/svg/217/217422.svg" style="height: 24px; width: 24px;"></a></center></th>							 
+</tr>
+</table>
 </footer>
