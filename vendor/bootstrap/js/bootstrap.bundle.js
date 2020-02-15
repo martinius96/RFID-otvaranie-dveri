02bc7f82dfd3fdeac9f00184d3af0654f2565020
Martin <martinius96@gmail.com>
2020-02-16 00:24:05 +0100

    Update bootstrap.bundle.js

--- vendor/bootstrap/js/bootstrap.bundle.js
+++ vendor/bootstrap/js/bootstrap.bundle.js
@@ -2,6 +2,7 @@
   * Bootstrap v4.1.3 (https://getbootstrap.com/)
   * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+  * MIT
   */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
