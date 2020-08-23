79906a4f5dc2f120e4d22074feecf53ad25cf0b6
Martin Chlebovec <martinius96@gmail.com>
2020-08-23 23:06:57 +0200

    OTA extension via LAN

--- examples/POST/HTTP_ESP8266_POST.ino
+++ examples/POST/HTTP_ESP8266_POST.ino
@@ -7,9 +7,17 @@
 /*|CORE: 2.3.0 - 2.5.0 compatible                            |*/
 /*|WEB: https://arduino.php5.sk                              |*/
 /*|----------------------------------------------------------|*/
+#define OTA //odkomentuj pre OTA UPDATE CEZ LAN
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <RFID.h>
+
+#ifdef OTA
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#endif
+
 const char * ssid = "MenoWifiSiete";
 const char * password = "HesloWifiSiete";
 const char * host = "www.arduino.php5.sk";
@@ -31,6 +39,35 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+#ifdef OTA 
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+#endif  
   Serial.println("");
   Serial.println("WiFi uspesne pripojene");
   Serial.println("IP adresa: ");
@@ -46,6 +83,9 @@ void loop() {
     delay(500);
     Serial.print(".");
   }
+#ifdef OTA 
+  ArduinoOTA.handle();
+#endif 
   if (rfid.isCard()) {
     if (rfid.readCardSerial()) {
       Serial.println(" ");
