8072c6ec9893d280745a3a62530c61f21978c95a
Martin Chlebovec <martinius96@gmail.com>
2020-08-23 23:10:37 +0200

    OTA via LAN support

--- examples/POST/HTTP_ESP32_POST.ino
+++ examples/POST/HTTP_ESP32_POST.ino
@@ -7,9 +7,17 @@
 /*|CORE: 1.0.0+                                              |*/
 /*|WEB: https://arduino.php5.sk                              |*/
 /*|----------------------------------------------------------|*/
+//#define OTA //odkomentuj pre OTA UPDATE CEZ LAN
 #include <WiFi.h>
 #include <SPI.h>
 #include <RFID.h>
+
+#ifdef OTA
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#endif
+
 const char * ssid = "wifi_meno";
 const char * password = "wifi_heslo";
 const char * host = "www.arduino.php5.sk";
@@ -31,6 +39,48 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+#ifdef OTA
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+#endif
   Serial.println("");
   Serial.println("WiFi uspesne pripojene");
   Serial.println("IP adresa: ");
@@ -46,6 +96,9 @@ void loop() {
     delay(500);
     Serial.print(".");
   }
+#ifdef OTA  
+  ArduinoOTA.handle();
+#endif
   if (rfid.isCard()) {
     if (rfid.readCardSerial()) {
       Serial.println(" ");
