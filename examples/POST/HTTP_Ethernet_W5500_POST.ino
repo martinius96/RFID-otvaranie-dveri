ea178ab84b5e4afbbc2905789462fcb705dc5e2d
Martin Chlebovec <martinius96@gmail.com>
2021-01-23 20:52:16 +0100

    Update HTTP_Ethernet_W5500_POST.ino

--- examples/POST/HTTP_Ethernet_W5500_POST.ino
+++ examples/POST/HTTP_Ethernet_W5500_POST.ino
@@ -4,14 +4,15 @@
 /*|FB: https://www.facebook.com/martin.s.chlebovec           |*/
 /*|EMAIL: martinius96@gmail.com                              |*/
 /*|Doska: Arduino Uno (Nano, Mega) + Wiznet W5500 (Ethernet) |*/
-/*|WEB: https://arduino.php5.sk                              |*/
+/*|Ethernet modul W5500 musi mat prepojene INT GPIO s D2     |*/
+/*|WEB: https://arduino.clanweb.eu                           |*/
 /*|----------------------------------------------------------|*/
 #include <RFID.h>
 #include <SPI.h>
 #include <Ethernet2.h>
-byte mac[] = { 0xAA, 0xBB, 0xCC, 0x81, 0x7B, 0x4A };
-const char * host = "www.arduino.php5.sk";
-IPAddress ip(192, 168, 1, 254);
+byte mac[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0x4A };
+const char * host = "arduino.clanweb.eu";
+IPAddress ip(192, 168, 1, 254); //STATIC IP
 #define SS_PIN 6
 #define RST_PIN 5
 RFID rfid(SS_PIN, RST_PIN);
@@ -25,33 +26,29 @@ void setup() {
   SPI.begin();
   rfid.init();
   if (Ethernet.begin(mac) == 0) {
+    // Ethernet.begin(mac);    //FOR DHCP
     Ethernet.begin(mac, ip);
   }
-  Serial.print("  DHCP assigned IP ");
+  Serial.print("Assigned IP ");
   Serial.println(Ethernet.localIP());
   Serial.println("Ready");
 }
 
 void loop() {
-  if (Ethernet.begin(mac) == 0) {
-    Ethernet.begin(mac, ip);
-    Serial.print("  DHCP assigned IP ");
-    Serial.println(Ethernet.localIP());
-  }
   if (rfid.isCard()) {
     if (rfid.readCardSerial()) {
       Serial.println(" ");
-      Serial.println("Card found");
+      Serial.print("Pouzivatel prilozil kartu: ");
       kod = 10000 * rfid.serNum[4] + 1000 * rfid.serNum[3] + 100 * rfid.serNum[2] + 10 * rfid.serNum[1] + rfid.serNum[0];
-      Serial.println(kod);
+      Serial.println("**********"); //nezverejni sa vo vypise moznemu utocnikovi
       client.stop();
       String kodik = String(kod);
       String data = "kod=" + kodik;
       String url = "/rfid/karta.php";
       if (client.connect(host, 80)) {
-        client.println("POST " + url + " HTTP/1.0");
+        client.println("POST " + url + " HTTP/1.0"); //pri HTTP/1.1 Chunk encoding - narocne spracovanie
         client.println("Host: " + (String)host);
-        client.println("User-Agent: ESP8266");
+        client.println("User-Agent: W5500");
         client.println("Connection: close");
         client.println("Content-Type: application/x-www-form-urlencoded;");
         client.print("Content-Length: ");
@@ -65,18 +62,21 @@ void loop() {
           }
         }
         String line = client.readStringUntil('\n');
-        if (line == "OK") {
+        if (line.indexOf("OK") > 0) {
+          Serial.println("VSTUP POVOLENY");
+          Serial.println("DVERE ODOMKNUTE");
           digitalWrite(rele, LOW); //invertovane spinane rele active LOW
           delay(5500);              //cas otvorenia dveri
           digitalWrite(rele, HIGH); //zatvor zamok
-        } else if (line == "NO") {
-          digitalWrite(rele, HIGH);
+          Serial.println("DVERE ZAMKNUTE");
+        } else if (line.indexOf("NO") > 0) {
+          Serial.println("VSTUP ZAMIETNUTY");
         } else {
           Serial.println("Prosim pockajte s dalsim overenim karty 5 sekund!");
         }
+        // Ethernet.maintain(); //ONLY FOR DHCP
       }
     }
   }
-
   rfid.halt();
 }
