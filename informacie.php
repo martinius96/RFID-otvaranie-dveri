dcc33c1e69e2aa9350b1239e6e16a734a70c20d6
Martin Chlebovec <martinius96@gmail.com>
2021-02-25 19:46:36 +0100

    Úprava popisov

--- informacie.php
+++ informacie.php
@@ -54,41 +54,56 @@ include("connect.php");
             <h1 id="demo"></h1>
 <h3>Informácie o projekte RFID vrátnika - ESP8266, ESP32, Arduino + Ethernet</h3>
 <div class="alert alert-danger">
-<strong>Projekt je šírený pod MIT licenciou. Prečítajte si obsah stránky licencia pre vedomosť o povinnostiach a obmedzeniach spojených s touto licenciou. Používaním projektu súhlasíte s MIT licenciou a uvedomujete si právne následky spojené s nedodržaním licencie.</strong>
+<strong>Projekt je šírený pod MIT licenciou. <font color="red">Prečítajte si obsah stránky licencia pre vedomosť o povinnostiach a obmedzeniach spojených s touto licenciou. Používaním projektu súhlasíte s MIT licenciou a uvedomujete si právne následky spojené s nedodržaním licencie v prípade jej porušenia!</font></strong>
 </div>   
 <hr>
 <div class="row">
-<div class="col-md-6">
- <h3>Administrátor: <img src="https://image.flaticon.com/icons/svg/236/236831.svg" width=64px height=64px></h3>
+<div class="col-md-12">
+<center><h3>Administrátor: <img src="https://image.flaticon.com/icons/svg/236/236831.svg" width=64px height=64px></h3></center>
 <hr>
-<li>Spravuje webové rozhranie</li>
-<li>V reálnom čase vidí aktivitu - priloženie karty vrátane jej adresy s časom jej priloženia s výsledkom overenia: OVERENÝ / NEOVERENÝ.</li>
-<li>Hlávný prehľad ponúka 100 posledných priložení karty, má výlučne informatívny charakter.</li>
-<li>V sekcii pridať kartu môže administrátor jedným kliknutím potvrdiť kartu - autorizovať ju, od tohto momentu je možné kartou odomknúť dvere.</li>
-<li>Okrem zobrazenia posledných 5 priložení karty vidí administrátor všetky neautorizované doteraz priložené karty a autorizované karty vrátane dátumov a časov ich autorizácie.</li>
-<li>V záložke Odobrania karty je možné deautozizovať autorizovanú kartu - s okamžitou platnosťou zrušiť jej autorizáciu, alebo ju je možné úplne odstrániť zo systému.</li>
-<li>V záložke Zapojenie môže administrátor nahliadnuť do schémy zapojenia RFID čítačky s podporovanými platformami -  NodeMCU s čipom ESP8266-12E, pre Devkit ESP32, či Arduino s Ethernet modulom rady Wiznet W5100 respektíve W5500.</li> 
-<li>Administrátor môže nahliadnuť a stiahnuť si zdrojový kód, ktorý je dostupný pre túto webovú lokalitu.</li>
-<li>Pre lepšiu prenositeľnosť programu medzi jednotlivými platformami používajú zdrojové kódy hardvérové SPI vývody tejto zbernice, čím je program možné kombinovať na rôzne platformy bez nutnosti zásahu (program pre Arduino je kompatibilný pre Arduino Uno, Nano, Mega 1280 / 2560.</li>
-<li>Možnosť prihlásenia, odhlásenia adminsitrátora, zmeny prihlasovacích údajov, núdzové otvorenie dverí z webového prostredia v prípade straty karty, priradenie zamestnancov, mien, fotografií a tlačenie reportov je súčasťou platenej verzie projektu. - vývoj platenej verzie je zastavený - neplánuje sa vydanie platenej verzie.</li>
-</div>
-<div class="col-md-6">
-<h3>Používateľ: <img src="https://www.flaticon.com/premium-icon/icons/svg/1610/1610320.svg" width=64px height=64px></h3>
+<p style="text-align: justify;">
+Spravuje webové rozhranie projektu RFID vrátnik.
+V reálnom čase vidí aktivitu - priloženie karty vrátane jej adresy (UID) s časovou značkou jej priloženia spolu s výsledkom overenia: <b><font color="#27AE60">OVERENÝ</font></b> / <b><font color="red">NEOVERENÝ</font></b>.
+Hlávný prehľad RFID vrátnika ponúka vo free verzii históriu posledných 100 priložení karty.
+V sekcii pridať kartu môže administrátor jedným kliknutím potvrdiť kartu - autorizovať ju, od tohto momentu je možné kartou odomknúť dvere, je autorizovaná, systém na ňu bude reagovať.
+Okrem zobrazenia posledných 5 priložení karty vidí administrátor všetky neautorizované doteraz priložené karty a autorizované karty vrátane dátumov a časov ich autorizácie, resp. záznamu.
+V záložke Odobrania karty je možné deautozizovať autorizovanú kartu - s okamžitou platnosťou zrušiť jej autorizáciu, alebo ju je možné úplne odstrániť zo systému.
+V záložke Zapojenie môže administrátor nahliadnuť do schémy zapojenia RFID čítačky NXP RC522 s podporovanými platformami -  NodeMCU s čipom ESP8266-12E, pre Devkit ESP32, či Arduino s Ethernet modulom rady Wiznet W5100 respektíve W5500.
+Administrátor môže nahliadnuť a stiahnuť si zdrojový kód, ktorý je dostupný pre túto webovú lokalitu pre platformy ESP8266, ESP32, Arduino + Ethernet.
+Pre lepšiu prenositeľnosť programu medzi jednotlivými platformami používajú zdrojové kódy hardvérové SPI vývody tejto zbernice, čím je program možné kombinovať na rôzne platformy bez nutnosti zásahu (program pre Arduino je kompatibilný pre Arduino Uno, Nano, Mega 1280 / 2560.
+<b>Možnosť prihlásenia, odhlásenia adminsitrátora, zmeny prihlasovacích údajov, núdzové otvorenie dverí z webového prostredia v prípade straty karty, priradenie zamestnancov, mien, fotografií a tlačenie reportov je súčasťou platenej verzie projektu.</b>
+</p>
 <hr>
-<li>Využíva služby RFID čítačky fyzickým priložením NFC tagu, kľúčenky, karty, náramku na 13.56MHz.</li>
-<li>V prípade overenia priloženého čipu mu NodeMCU odistí dvere na 5 a pol sekundy, počas ktorých musí používateľ dvere otvoriť. V opačnom prípade sa uzamknú a používateľ musí opakovať priloženie karty.</li>
-<li>Pri neoverení používateľa z dôvodu neovereného čipu, prípadne nedostupnosti overenia webu mu zámok nie je odistený - nevtiahne sa, dvere nie je možné otvoriť.</li>
-<li>Používateľ o neoverení svojho čipu ako takého nie je informovaný. Jeho pokus je zapísaný do databázy za predpokladu, že sa ESP8266 (ESP32, Arduino) napojí na web, kde sa karta overuje.</li>
-<li>Používateľ môže na overenie použiť aj svoju bankomatovú kartu, ISIC kartu, či zamestnaneckú kartu, ktorej fyzickú adresu čítačka dokáže prečítať, musí spĺňať štandard ISO/IEC 14443 A a pracovnú frekvenciu 13.56MHz.</li>
-</div>
-</div>
+<center><h3>Používateľ: <img src="https://www.flaticon.com/premium-icon/icons/svg/3135/3135715.svg" width=64px height=64px></h3></center>
 <hr>
-<li>Čítačka RC522 pracuje na frekvencii 13.56MHz, využíva SPI zbernicu pre rýchlu komunikáciu s mikrokontrolérom</li>
-<li>Implementačné demo využíva trojicu platforiem - ESP8266, ESP32, Arduino + Ethernet na prepojenie s touto čítačkou pre dosiahnutie optimálnej funkčnosti.</li>
-<li><b><font color="red">Čítačka RC522 využíva už prelomený algoritmus Crypto-1</font>, nie je preto vhodná pre zabezpečenie cenného majetku.</b></li>
-<li>Pre zvýšenie bezpečnosti tejto implementácie sa používa konverzia UID identifikátorov RFID kariet na strane mikrokontroléru po načítaní, druhotná konverzia sa vykoná na strane webového servera, kedy je výsledná adresa karty hashuje algoritmom CRC32B, tento hash je uložený do databázy a reprezentuje danú kartu.</li>
-<li>Čítačka má vstavanú anténu, priloženie karty vie registrovať na cca. 3 centimetre od kontaktnej plochy. </li>
-<li>V kľúdovom stave čítačka spotrebuje 13mA pri 3.3V, pri načítavaní kariet (v prevádzke) je prúdovú odber na hranici 26mA pri 3.3V napájaní.</li>
+<p style="text-align: justify;">
+Využíva služby RFID čítačky NXP RC522 fyzickým priložením karty, náramku, kľúčenky kompatibilného štandardu ISO/IEC 14443-A.
+V prípade overenia priloženého čipu mu mikrokontróler odistí dvere na 5 a pol sekundy (vtiahne západku), počas ktorých musí používateľ dvere otvoriť. 
+V opačnom prípade sa uzamknú a používateľ musí opakovať priloženie karty. Systém reaguje na nové priloženie karty až po 5-tich sekundách od priloženia karty v predchádzajúcom pokuse.
+Pri neoverení používateľa z dôvodu neovereného čipu, prípadne nedostupnosti overenia webu mu zámok nie je odistený - nevtiahne sa, dvere nie je možné otvoriť (používateľ ako taký nemá vedomosť o výsledku overenia).
+Používateľ o neoverení svojho čipu ako takého nie je informovaný. Jeho pokus je zapísaný do databázy za predpokladu, že sa ESP8266 (ESP32, Arduino) napojí na web, kde sa karta overuje.
+Používateľ môže na overenie použiť aj svoju ISIC, autobusovú kartu, ktorej fyzickú UID adresu čítačka dokáže prečítať, musí spĺňať štandard ISO/IEC 14443 A.
+</p>
+<hr>
+<center><h3>Technické informácie: <img src="https://www.flaticon.com/premium-icon/icons/svg/1326/1326318.svg" width=64px height=64px></h3></center>
+<hr>
+<p style="text-align: justify;">
+Čítačka NXP RC522, ktoráje v projekte použitá pracuje na frekvencii 13.56MHz.
+V tomto projekte čítačka načítava iba fyzickú adresu (UID) RFID kariet, kľúčeniek náramkov, ktoré sa sami odhlásia RFID čítačke, ktorá ich nabije elektrickým poľom.
+Pre komunikáciu s mikrokontrolérom využíva čítačka NXP RC522 SPI zbernicu.
+V projekte sú dostupné programové implementácie pre ESP8266, ESP32, Arduino + Ethernet pre komunikáciu s RFID čítačkou a prenosom zaznamenaných údajov na vzdialené webové rozhranie.
+Mikrokontrolér využíva HTTP (HTTPS - iba ESP8266 a ESP32 má aj túto možnosť) POST request, pričom má v tele správy payload z RFID čítačky.
+Neposiela sa reálne UID, mikrokontróler vykoná jeho korekciu - pozmenenie.
+Po odoslaní dát do webového rozhrania sa vykoná checksum cez CRC32B na strane webservera.
+Pod týmto checksumom vystupuje UID v systéme a je to jeho identifikátor v celom webovom rozhraní.
+<b><font color="red">Týmto projektom nie je odporúčané zabezpečovať majetok</font>, za škody, stratu dát autor projektu nezodpovedá.</b>
+</p>
+<div class="alert alert-danger">
+      <strong>Pozor na RFID čítačky NXP RC522 s falošným (neoriginálnym NXP RC522) čipom! - V projekte na UART hlási: Firmware version: 0x12 - counterfeit chip. Projekt s falošným čipom nefuinguje. </strong>
+	Testované a funkčné s NXP RFID čítačkou RC522, ktorá hlási verziu firmvéru: Firmware version 0x92 (Version 2.0).
+      </div>
+</div>
+</div>
 <hr>
 </div>   
       <?php 
