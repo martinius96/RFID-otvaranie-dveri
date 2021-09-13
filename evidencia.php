9eee2d29e56d4d802f7738648ea402d772233256
Martin Chlebovec <martinius96@gmail.com>
2021-09-13 10:35:03 +0200

    Update evidencia.php

--- evidencia.php
+++ evidencia.php
@@ -51,7 +51,7 @@ include("connect.php");
     <!-- Page Content -->
     <div class="container">
       <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-12 text-center">
       <h1 id="demo"></h1>
 <h3>Evidencia nových (neregistrovaných) zamestnancov</h3>
 <hr>
