ad1ea223129de7cfaa841673ab9ca56226877ec7
Martin Chlebovec <martinius96@gmail.com>
2021-12-09 01:21:31 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,6 @@
 <?php 
+//UDAJE PRE PRIPOJENIE K DATABAZE
+//parameters order: host, username, password, database_name
 $con = mysqli_connect("localhost/EXTERNAL_DB_HOST","pouzivatel_db_root","heslo_do_databazy","pouzivana_databaza");
 mysqli_set_charset($con, "utf8");
 if (mysqli_connect_errno($con))
