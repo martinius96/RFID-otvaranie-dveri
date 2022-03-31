9f9d16bb07e58d3fb7b40bbf4efa02bea09f7f75
Martin Chlebovec <martinius96@gmail.com>
2022-03-31 02:51:51 +0200

    FreeRTOS, changed library, no OTA

--- examples/POST/HTTPS_ESP32_POST.ino
+++ examples/POST/HTTPS_ESP32_POST.ino
@@ -1,180 +1,188 @@
-/*|----------------------------------------------------------|*/
-/*|SKETCH PRE RFID SYSTEM S WEB ADMINISTRACIOU               |*/
-/*|VYHOTOVIL: MARTIN CHLEBOVEC                               |*/
-/*|FB: https://www.facebook.com/martin.s.chlebovec           |*/
-/*|EMAIL: martinius96@gmail.com                              |*/
-/*|Doska: ESP32 DevKit V1 - V4                               |*/
-/*|CORE: 1.0.0+                                              |*/
-/*|WEB: https://arduino.php5.sk                              |*/
-/*|----------------------------------------------------------|*/
-//#define OTA //odkomentuj pre OTA UPDATE CEZ LAN
+/*|--------------------------------------------------------|*/
+/*|Project: RFID remote database access webapp             |*/
+/*|ESP32 (DevKit, Generic), Arduino Core with FreeRTOS     |*/
+/*|Autor: Martin Chlebovec (martinius96)                   |*/
+/*|E-mail: martinius96@gmail.com                           |*/
+/*|There isn't test web interface available for HTTPS      |*/
+/*|Usage license: MIT                                      |*/
+/*|Revísion: 31. March 2022                                |*/
+/*|Tested stable ESP32 core: 2.0.2                         |*/
+/*|799 kB flash usage, 38 kB RAM usage                     |*/
+/*|--------------------------------------------------------|*/
+
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <SPI.h>
-#include <RFID.h>
+#include <MFRC522.h>
 
-#ifdef OTA
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-#endif
+unsigned long code;
+const int rele = 17; //RELAY PIN
+#define SS_PIN 21 //CHIP SELECT PIN
+#define RST_PIN 22 //RESET PIN
 
-const char * ssid = "WIFI_NAME";
-const char * password = "WIFI_PASSWORD";
-const char * host = "arduino.php5.sk"; //POUZITA DOMENA NEEXISTUJE. PRIPOJENIE SA NEVYKONA.
-const int httpsPort = 443; //http port
-const int rele = 17;
-#define SS_PIN 21
-#define RST_PIN 22
-RFID rfid(SS_PIN, RST_PIN);
-unsigned long kod;
-//Root CA certifikát JE MOZNE ZISKAT CEZ NASTROJ OpenSSL prikazom:
-//openssl s_client -showcerts -verify 5 -connect example.com:443 < /dev/null
-//POUZITE VZDY ROOT CA (nie Intermediate CA), format .pem
-const char* test_root_ca = \
-                           "-----BEGIN CERTIFICATE-----\n" \
-                           "MIIEsTCCA5mgAwIBAgIQCKWiRs1LXIyD1wK0u6tTSTANBgkqhkiG9w0BAQsFADBh\n" \
-                           "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
-                           "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
-                           "QTAeFw0xNzExMDYxMjIzMzNaFw0yNzExMDYxMjIzMzNaMF4xCzAJBgNVBAYTAlVT\n" \
-                           "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
-                           "b20xHTAbBgNVBAMTFFJhcGlkU1NMIFJTQSBDQSAyMDE4MIIBIjANBgkqhkiG9w0B\n" \
-                           "AQEFAAOCAQ8AMIIBCgKCAQEA5S2oihEo9nnpezoziDtx4WWLLCll/e0t1EYemE5n\n" \
-                           "+MgP5viaHLy+VpHP+ndX5D18INIuuAV8wFq26KF5U0WNIZiQp6mLtIWjUeWDPA28\n" \
-                           "OeyhTlj9TLk2beytbtFU6ypbpWUltmvY5V8ngspC7nFRNCjpfnDED2kRyJzO8yoK\n" \
-                           "MFz4J4JE8N7NA1uJwUEFMUvHLs0scLoPZkKcewIRm1RV2AxmFQxJkdf7YN9Pckki\n" \
-                           "f2Xgm3b48BZn0zf0qXsSeGu84ua9gwzjzI7tbTBjayTpT+/XpWuBVv6fvarI6bik\n" \
-                           "KB859OSGQuw73XXgeuFwEPHTIRoUtkzu3/EQ+LtwznkkdQIDAQABo4IBZjCCAWIw\n" \
-                           "HQYDVR0OBBYEFFPKF1n8a8ADIS8aruSqqByCVtp1MB8GA1UdIwQYMBaAFAPeUDVW\n" \
-                           "0Uy7ZvCj4hsbw5eyPdFVMA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEF\n" \
-                           "BQcDAQYIKwYBBQUHAwIwEgYDVR0TAQH/BAgwBgEB/wIBADA0BggrBgEFBQcBAQQo\n" \
-                           "MCYwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBCBgNVHR8E\n" \
-                           "OzA5MDegNaAzhjFodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vRGlnaUNlcnRHbG9i\n" \
-                           "YWxSb290Q0EuY3JsMGMGA1UdIARcMFowNwYJYIZIAYb9bAECMCowKAYIKwYBBQUH\n" \
-                           "AgEWHGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCwYJYIZIAYb9bAEBMAgG\n" \
-                           "BmeBDAECATAIBgZngQwBAgIwDQYJKoZIhvcNAQELBQADggEBAH4jx/LKNW5ZklFc\n" \
-                           "YWs8Ejbm0nyzKeZC2KOVYR7P8gevKyslWm4Xo4BSzKr235FsJ4aFt6yAiv1eY0tZ\n" \
-                           "/ZN18bOGSGStoEc/JE4ocIzr8P5Mg11kRYHbmgYnr1Rxeki5mSeb39DGxTpJD4kG\n" \
-                           "hs5lXNoo4conUiiJwKaqH7vh2baryd8pMISag83JUqyVGc2tWPpO0329/CWq2kry\n" \
-                           "qv66OSMjwulUz0dXf4OHQasR7CNfIr+4KScc6ABlQ5RDF86PGeE6kdwSQkFiB/cQ\n" \
-                           "ysNyq0jEDQTkfa2pjmuWtMCNbBnhFXBYejfubIhaUbEv2FOQB3dCav+FPg5eEveX\n" \
-                           "TVyMnGo=\n" \
-                           "-----END CERTIFICATE-----\n";
+const char * ssid = "MY_WIFI"; //WiFi hotspot name
+const char * password = "MY_WIFI_PASSWORD"; //WiFi hotspot password
+const char* host = "XXXXX.com"; //domain - host
+String url = "/rfid/karta.php"; //URL behind host domain --> target PHP file
+
+
+
+TaskHandle_t Task1; //ULTRASONIC MEASUREMENT
+TaskHandle_t Task2; //WIFI HTTP SOCKET
+QueueHandle_t  q = NULL;
+
 WiFiClientSecure client;
+MFRC522 rfid(SS_PIN, RST_PIN);
+static void Task1code( void * parameter);
+static void Task2code( void * parameter);
+void dump_byte_array(byte *buffer, byte bufferSize);
+
+//DST ROOT CA X3 EXAMPLE (https://i.imgur.com/fvw4huT.png)
+const static char* test_root_ca PROGMEM = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+    "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+    "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+    "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+    "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+    "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+    "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+    "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+    "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+    "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+    "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+    "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+    "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+    "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+    "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+    "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+    "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+    "-----END CERTIFICATE-----\n";
+
 void setup() {
   Serial.begin(115200);
   SPI.begin();
-  rfid.init();
+  rfid.PCD_Init(); // Init MFRC522
+  rfid.PCD_SetAntennaGain(112); // set antenna gain to max (00001110)
+  rfid.PCD_DumpVersionToSerial(); //Write version of Firmware to UART
   pinMode(rele, OUTPUT);
   digitalWrite(rele, HIGH); //hotfix
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.print(".");
   }
-  client.setCACert(test_root_ca);
-#ifdef OTA
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
-  ArduinoOTA.begin();  
-#endif
-  Serial.println("");
-  Serial.println("WiFi uspesne pripojene");
-  Serial.println("IP adresa: ");
+  Serial.println(F(""));
+  Serial.println(F("Wifi connected with IP:"));
   Serial.println(WiFi.localIP());
-  Serial.println("Ready");
+  client.setCACert(test_root_ca);
+  q = xQueueCreate(20, sizeof(unsigned long));
+  if (q != NULL) {
+    Serial.println(F("Queue FIFO buffer is created"));
+    vTaskDelay(1000 / portTICK_PERIOD_MS); //wait for a second
+    xTaskCreatePinnedToCore(
+      Task1code,   /* Task function. */
+      "Task1",     /* name of task. */
+      10000,       /* Stack size of task */
+      NULL,        /* parameter of the task */
+      1,           /* priority of the task */
+      &Task1,      /* Task handle to keep track of created task */
+      1);          /* pin task to core 1 */
+    Serial.println(F("RFID handler task started"));
+    xTaskCreatePinnedToCore(
+      Task2code,   /* Task function. */
+      "Task2",     /* name of task. */
+      10000,       /* Stack size of task */
+      NULL,        /* parameter of the task */
+      1,           /* priority of the task */
+      &Task2,      /* Task handle to keep track of created task */
+      0);          /* pin task to core 0 */
+    Serial.println(F("HTTP Socket task started"));
+  } else {
+    Serial.println(F("Queue creation failed"));
+  }
 }
 
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
   }
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-#ifdef OTA  
-  ArduinoOTA.handle();
-#endif
-  if (rfid.isCard()) {
-    if (rfid.readCardSerial()) {
-      Serial.println(" ");
-      Serial.println("Card found");
-      kod = 10000 * rfid.serNum[4] + 1000 * rfid.serNum[3] + 100 * rfid.serNum[2] + 10 * rfid.serNum[1] + rfid.serNum[0];
-    //  Serial.println(kod);
-      client.stop();
-      String kodik = String(kod);
-      String data = "kod=" + kodik;
-      String url = "/rfid/karta.php";
-      if (client.connect(host, httpsPort)) {
-        client.println("POST " + url + " HTTP/1.0");
-        client.println("Host: " + (String)host);
-        client.println("User-Agent: ESP32");
-        client.println("Connection: close");
-        client.println("Content-Type: application/x-www-form-urlencoded;");
-        client.print("Content-Length: ");
-        client.println(data.length());
-        client.println();
-        client.println(data);
-        while (client.connected()) {
-          String line = client.readStringUntil('\n');
-          if (line == "\r") {
-            break;
-          }
-        }
-        String line = client.readStringUntil('\n');
-        if (line.indexOf("OK") > 0) {
-          Serial.println(F("VSTUP POVOLENY"));
-          Serial.println(F("DVERE ODOMKNUTE"));
-          digitalWrite(rele, LOW); //invertovane spinane rele active LOW
-          delay(5500);              //cas otvorenia dveri
-          digitalWrite(rele, HIGH); //zatvor zamok
-          Serial.println(F("DVERE ZAMKNUTE"));
-        } else if (line.indexOf("NO") > 0) {
-          Serial.println(F("VSTUP ZAMIETNUTY"));
-        } else {
-          Serial.println(F("Prosim pockajte s dalsim overenim karty 5 sekund!"));
-        }
-      }
+  yield();
+
+}
+
+static void Task1code( void * parameter) {
+  unsigned long RFIDcode;
+  if (q == NULL) {
+    Serial.println(F("Queue in RFID handler task is not ready"));
+    return;
+  }
+  while (1) {
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+      dump_byte_array(rfid.uid.uidByte, rfid.uid.size);
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+      RFIDcode = code;
+      xQueueSend(q, (void *)&RFIDcode, (TickType_t )0); //add the measurement value to Queue
     }
   }
+}
+static void Task2code( void * parameter) {
+  unsigned long RFIDcode;
+  if (q == NULL) {
+    Serial.println(F("Queue in HTTP socket task is not ready"));
+    return;
+  }
+  while (1) {
+    xQueueReceive(q, &RFIDcode, portMAX_DELAY); //read measurement value from Queue and run code below, if no value, WAIT until portMAX_DELAY
+    String data = "kod=" + RFIDcode;
+    if (client.connect(host, 443)) {
+      client.println("POST " + url + " HTTP/1.0");
+      client.println("Host: " + (String)host);
+      client.println("User-Agent: ESP32");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded;");
+      client.print("Content-Length: ");
+      client.println(data.length());
+      client.println();
+      client.println(data);
+      while (client.connected()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          break;
+        }
+      }
+      String line = client.readStringUntil('\n');
+      if (line.indexOf("OK") > 0) {
+        Serial.println(F("ACCESS GAINED"));
+        Serial.println(F("DOORS OPENED"));
+        digitalWrite(rele, LOW); //inverted ACTIVE-LOW logic
+        delay(5500);              //duration of doors opened
+        digitalWrite(rele, HIGH); //lock doors pack
+        Serial.println(F("DOORS CLOSED"));
+      } else if (line.indexOf("NO") > 0) {
+        Serial.println(F("ACCESS REJECTED"));
+      } else {
+        Serial.println(F("WAIT 5 seconds before next attempt!"));
+      }
+    } else {
+      Serial.println(F("Connection to webserver was not successful!"));
+    }
+    client.stop();
+  }
+}
 
-  rfid.halt();
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+  }
+  Serial.print(F("Detected UID (code) of RFID CARD: "));
+  code = 10000 * buffer[4] + 1000 * buffer[3] + 100 * buffer[2] + 10 * buffer[1] + buffer[0]; //finalny kod karty
+  Serial.println(code);
 }
